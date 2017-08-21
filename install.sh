3a2e1ca4179932433e1a0ee6184915e9c469dff4
Hamza Basrai <hamza.basrai@gmail.com>
2017-08-20 21:55:03 -0400

    Removed vim setup and added install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+files=".bash_aliases .bash_exports .bash_options .bash_prompt .bashrc .gitignore .gitconfig .gitattributes .hushlogin .inputrc .profile"
+
+echo "Backing up files"
+mkdir -p ~/dotfiles_old
+
+cd ~/dotfiles
+
+for file in $files; do
+  if [ -f ~/$file ]; then
+    mv ~/$file ~/dotfiles_old
+    echo "$file backed up"
+  fi
+done
+
+for file in $files; do
+  ln -s ~/dotfiles/$file ~/$file
+  echo "Linking $file"
+done
+
+source ~/.profile
