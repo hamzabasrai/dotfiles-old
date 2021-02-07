ad179ce8c75f3c580bae5221831f980e75917794
Hamza Basrai <hamza.basrai@gmail.com>
2021-02-07 17:32:04 -0500

    Move aliases into seperate file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+alias g="git"
+alias m="man"
+alias path='printf "%b\n" "${PATH//:/\\n}"'
+alias v="vim"
+
+# Moving Files
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias rm='rm -i'
+
+# Miscellaneous 
+alias h="history"
+alias c="clear"
